e6f2c8780e4de114cf520c12110a4af9d4140cc6
frankline <Ugwu.nnaemekan@gmail.com>
2014-10-15 13:27:51 +0100

    user encryption now working

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,5 +1,5 @@
 class AccessController < ApplicationController
-  
+  layout 'admin'
   def index
   	#display text and links
   end
@@ -9,7 +9,21 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
-
+  	if params[:username].present? && params[:password].present?
+      found_user = User.where(:username => params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate( params[:password]) #User.authenticate(params[:username], params[:password])
+      end
+    end
+    if authorized_user
+      # TODO: mark user as logged in
+      flash[:notice] = "You are now logged in."
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Invalid username/password combination."
+      redirect_to(:action => 'login')
+    end
+  	
   end
 
   def logout
