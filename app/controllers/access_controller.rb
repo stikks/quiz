535f05f28f4e2e4232659bfe70fdb374785514e5
frankline <Ugwu.nnaemekan@gmail.com>
2014-10-15 11:54:44 +0100

    added login and logout controller not formatted yet though

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,18 @@
+class AccessController < ApplicationController
+  
+  def index
+  	#display text and links
+  end
+
+  def login
+  	# login form
+  end
+
+  def attempt_login
+
+  end
+
+  def logout
+   	
+  end 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
 	#07 10
 	#scope :visible, lambda {wherem(:active => true)} 
 	scope :sorted, lambda{ order("users.id ASC")}
