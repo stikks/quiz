9e70844829b9a77ed0d404d557a1e41a4f029510
frankline <Ugwu.nnaemekan@gmail.com>
2014-10-15 16:05:01 +0100

    working app with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015095306) do
+ActiveRecord::Schema.define(version: 20141015145843) do
+
+  create_table "admin_users", force: true do |t|
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
